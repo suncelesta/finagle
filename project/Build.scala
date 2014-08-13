a385264b74ac500a98a5507e7bc849f8886e0178
suncelesta <suncelesta@gmail.com>
2014-08-13 18:55:02 +0400

    Resolved merge conflict and optimized imports

--- project/Build.scala
+++ project/Build.scala
@@ -362,7 +362,7 @@ object Finagle extends Build {
       sharedSettings
   ).settings(
       name := "finagle-smtp",
-      libraryDependencies ++= Seq(util("logging"), util("codec"))
+      libraryDependencies ++= Seq(util("logging"))
     ).dependsOn(finagleCore)
 
 /*  notyet
