81f244995bce19345c6db3cdd6e072e5268ab678
suncelesta <suncelesta@gmail.com>
2014-11-25 01:30:44 +0300

    Switched to StackClient

--- finagle-example/src/main/scala/com/twitter/finagle/example/smtp/Example.scala
+++ finagle-example/src/main/scala/com/twitter/finagle/example/smtp/Example.scala
@@ -1,30 +1,34 @@
 package com.twitter.finagle.example.smtp
 
-import com.twitter.finagle.{SmtpSimple, smtp}
-import com.twitter.finagle.smtp.EmailBuilder
+import com.twitter.finagle.Smtp
+import com.twitter.finagle.smtp._
+import com.twitter.logging.Logger
 import com.twitter.util.{Await, Future}
 
 /**
  * Simple SMTP client with an example of error handling
  */
 object Example {
-  def main(args: Array[String]) = {
-    //raw text email
-    val email = EmailBuilder()
-                .sender("from@from.com")
-                .to("first@to.com", "second@to.com")
-                .subject("test")
-                .text("text")
-                .build
-    //connect to a local SMTP server
-    val send = SmtpSimple.newService("localhost:25")
-    //send email
-    val res: Future[Unit] = send(email)
-      .onFailure {
-      //An error group
-      case ex: smtp.reply.SyntaxErrorReply => println("Syntax error: ", ex.info)
-      //A concrete reply
-      case smtp.reply.ProcessingError(info) => println("Error processing request: ", info)
+  private val log = Logger.get(getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+    // Raw text email
+    val email = DefaultEmail()
+      .from_("from@from.com")
+      .to_("first@to.com", "second@to.com")
+      .subject_("test")
+      .text("first line", "second line") //body is a sequence of lines
+
+    // Connect to a local SMTP server
+    val send = Smtp.client.simple.newService("localhost:2525")
+
+    // Send email
+    val res: Future[Unit] = send(email) onFailure {
+    // An error group
+    case ex: SyntaxErrorReply => log.error("Syntax error: %s", ex.info)
+
+    // A concrete reply
+    case ProcessingError(info) => log.error("Error processing request: %s", info)
     }
 
     println("Sending email...") //this will be printed before the future returns
