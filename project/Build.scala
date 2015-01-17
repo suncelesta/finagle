twitter/twitter-server

[split] release finagle version 6.13.1
RB_ID=317383
TBR=true
NO-QUEUE=true

project/Build.scala
@@ -7,7 +7,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object TwitterServer extends Build {
   val libVersion = "1.6.0"
   val utilVersion = "6.13.2"
-  val finagleVersion = "6.13.0"
+  val finagleVersion = "6.13.1"
   val jacksonVersion = "2.2.2"
 
   val jacksonLibs = Seq(

suncelesta/finagle

project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.13.0"
+  val libVersion = "6.13.1"
   val zkVersion = "3.3.4"
   val utilVersion = "6.13.2"
   val ostrichVersion = "9.4.2"