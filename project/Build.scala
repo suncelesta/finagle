twitter-archive/ostrich

[split] release ostrich version 9.4.2
RB_ID=317283
TBR=true
NO-QUEUE=true

project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 import Tests._
 
 object Ostrich extends Build {
-  val libVersion = "9.4.1"
+  val libVersion = "9.4.2"
   val utilVersion = "6.13.2"
 
   val sharedSettings = Seq(

suncelesta/finagle

project/Build.scala
@@ -9,7 +9,7 @@ object Finagle extends Build {
   val libVersion = "6.13.0"
   val zkVersion = "3.3.4"
   val utilVersion = "6.13.2"
-  val ostrichVersion = "9.4.1"
+  val ostrichVersion = "9.4.2"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.8.1.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % ostrichVersion