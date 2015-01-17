twitter/twitter-server

[split] bumping finagle to 6.16.0
RB_ID=358591
TBR=true

project/Build.scala
@@ -7,7 +7,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object TwitterServer extends Build {
   val libVersion = "1.6.3"
   val utilVersion = "6.16.0"
-  val finagleVersion = "6.15.0"
+  val finagleVersion = "6.16.0"
   val jacksonVersion = "2.2.2"
 
   val jacksonLibs = Seq(

suncelesta/finagle

project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.15.0"
+  val libVersion = "6.16.0"
   val zkVersion = "3.3.4"
   val utilVersion = "6.16.0"
   val ostrichVersion = "9.5.0"