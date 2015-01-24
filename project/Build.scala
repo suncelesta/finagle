twitter/twitter-server

[split] csl: publish the world
RB_ID=450951
TBR=true

project/Build.scala
@@ -5,9 +5,9 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object TwitterServer extends Build {
-  val libVersion = "1.7.3"
-  val utilVersion = "6.21.1"
-  val finagleVersion = "6.20.0"
+  val libVersion = "1.7.6"
+  val utilVersion = "6.21.2"
+  val finagleVersion = "6.21.0"
   val jacksonVersion = "2.3.1"
   val mustacheVersion = "0.8.12.1"
 

suncelesta/finagle

project/Build.scala
@@ -6,9 +6,9 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.22.0"
+  val libVersion = "6.21.0"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.21.1"
+  val utilVersion = "6.21.2"
   val ostrichVersion = "9.5.6"
   val jacksonVersion = "2.3.1"
   val nettyLib = "io.netty" % "netty" % "3.9.1.1.Final"