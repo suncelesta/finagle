twitter/twitter-server

[split] bump all the things
RB_ID=422405
TBR=true

project/Build.scala
@@ -5,9 +5,9 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object TwitterServer extends Build {
-  val libVersion = "1.7.2"
-  val utilVersion = "6.18.2"
-  val finagleVersion = "6.19.0"
+  val libVersion = "1.7.3"
+  val utilVersion = "6.18.3"
+  val finagleVersion = "6.20.0"
   val jacksonVersion = "2.3.1"
   val mustacheVersion = "0.8.12.1"
 

suncelesta/finagle

project/Build.scala
@@ -6,10 +6,10 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.19.0"
+  val libVersion = "6.20.0"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.18.2"
-  val ostrichVersion = "9.5.4"
+  val utilVersion = "6.18.3"
+  val ostrichVersion = "9.5.5"
   val jacksonVersion = "2.3.1"
   val nettyLib = "io.netty" % "netty" % "3.9.1.1.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % ostrichVersion